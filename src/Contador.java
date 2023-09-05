9d097751feb335e3e6223ed9dd9f91b74122a5c1
Lucas <lucas_dev@outlook.com.br>
2023-09-05 14:09:16 -0300

    Projeto finalizado

--- src/Contador.java
+++ src/Contador.java
@@ -9,6 +9,24 @@ public class Contador {
         System.out.println("Digite o segundo parâmetro: ");
         int parametroDois = sc.nextInt();
 
+        try {
+            contar(parametroUm, parametroDois);
 
+        } catch (ParametrosInvalidosException e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
+
+        sc.close();
+
+    }
+
+    static void contar (int parametroUm, int parametrodois) throws ParametrosInvalidosException{
+        if(parametroUm > parametrodois)
+            throw new ParametrosInvalidosException("O segundo parametro deve ser maior que o primeiro");
+
+        int contagem = parametrodois - parametroUm;
+        for (int i = 1; i <= contagem; i++){
+            System.out.println("Imprimindo o numero : "+ i);
+        }
     }
 }
--- src/ParametrosInvalidosException.java
+++ src/ParametrosInvalidosException.java
@@ -1,5 +1,5 @@
 public class ParametrosInvalidosException extends Exception{
-    public ParametrosInvalidosException (String Mensagem){
-        super(Mensagem);
+    public ParametrosInvalidosException (String message){
+        super(message);
     }
 }
